600f87d06e41f7f31f1ead36c3948e29739c69d7
Victor Alexandre <varm@ic.ufal.br>
2025-03-02 23:55:05 -0300

    feat: cria enums para tokens reconhecidos pelo scanner

---
+++ src/enums/TokenType.h
@@ -0,0 +1,59 @@
+/**
+ * @file        TokenType.h
+ * @brief       Defines the TokenType enum class for the scanner.
+ * @author      Victor Miranda <victoralexandre499@gmail.com>
+ * @version     1.0.0
+ * @date        02/03/2025
+ */
+
+/**
+ * @brief All token types recognized by the scanner.
+ */
+enum class TokenType {
+  /* Single-character tokens. */
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  /* One or two character tokens. */
+  BANG,
+  BANG_EQUAL,
+  EQUAL_ASSIGN,
+  EQUAL_COMPARE,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  /* Literals. */
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  /* Keywords. */
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+  EOF
+};
